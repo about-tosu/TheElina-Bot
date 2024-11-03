813c45c000bec07a98205eb4d25892ed4d676dce
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-11-04 02:42:21 +0530

    Update notes.py

--- DewmiBot/modules/notes.py
+++ DewmiBot/modules/notes.py
@@ -545,7 +545,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "NOTES📖 "
+__mod_name__ = "NOTES"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
