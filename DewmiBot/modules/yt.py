79f55bbb580d5fd4f02d13d8aba9b77001e47fda
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-11-04 03:14:57 +0530

    Update yt.py

--- DewmiBot/modules/yt.py
+++ DewmiBot/modules/yt.py
@@ -3,6 +3,6 @@ __help__ = """
  ❍ /yt <text>: perform a youtube search
  ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server.
  
-   ©️@TheElinaBot🇱🇰
+   @Elina_Roxbot
 """
-__mod_name__ = "YOUTUBE🥌"
+__mod_name__ = "YOUTUBE"
